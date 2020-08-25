f92114c161294bb177db66257816e3537f176cb2
colew-picaro <colevohs@gmail.com>
2020-08-25 10:41:44 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace compilers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Token.cs
@@ -0,0 +1,10 @@
+class Token {
+  private TokenType type;
+  private string lexeme;
+
+  public Token (TokenType type, string lexeme) {
+    this.type = type;
+    this.lexeme = lexeme;
+  }
+
+}
