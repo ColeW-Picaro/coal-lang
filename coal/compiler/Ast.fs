6692eb1af4c5ff14cf1142b7e5cc4249087e2e00
Will Killian <william.killian@outlook.com>
2020-09-12 13:37:00 +0000

    Initial parse working

--- coal/compiler/Ast.fs
+++ coal/compiler/Ast.fs
@@ -7,46 +7,47 @@ type Type =
   | StringType
   | NilType
 
-
 type Binary =
-| OpNotEqual
-| OpGreaterEqual
-| OpLessEqual
-| OpEqual
-| OpAnd
-| OpOr
-| OpLess
-| OpGreater
-| OpMul
-| OpDiv
-| OpPlus
-| OpMinus
+  | OpNotEqual
+  | OpGreaterEqual
+  | OpLessEqual
+  | OpEqual
+  | OpAnd
+  | OpOr
+  | OpLess
+  | OpGreater
+  | OpMul
+  | OpDiv
+  | OpPlus
+  | OpMinus
 
 type Unary =
-| OpBoolNegate
-| OpValNegate
-| OpIncr
-| OpDecr
+  | OpBoolNegate
+  | OpValNegate
+  | OpIncr
+  | OpDecr
 
-type Formal =  string * Type
+type Formal =
+  | Formal of string * Type
 
 type Expr = 
- | Val of string 
- | Int of int
- | Float of double
- | String of string
- | FuncCall of string * Expr list
- | BinOp of Expr * Binary * Expr
- | UnOp of Unary * Expr
-
+  | VarRef of string 
+  | Int of int
+  | Float of double
+  | String of string
+  | Bool of bool
+  | FuncCall of string * Expr list
+  | BinOp of Expr * Binary * Expr
+  | UnOp of Unary * Expr
 
 type Stmt = 
- | Assign of string * Expr
- | While of Expr * Stmt 
- | Seq of Stmt list
- | IfThenElse of Expr * Stmt * Stmt option
- | Vardef of Formal * Expr option
- | Funcdef of Formal * Formal list option * Stmt
- | Expr of Expr
+  | Assign of string * Expr
+  | While of Expr * Stmt 
+  | Seq of Stmt list
+  | IfThenElse of Expr * Stmt * Stmt option
+  | Vardef of Formal * Expr option
+  | Funcdef of Formal * Formal list * Stmt
+  | Expr of Expr
 
-type Prog = Stmt list
+type Prog =
+  | Prog of Stmt list
